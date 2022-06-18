cc1b9a6efd832d606c4dd6b327f035c46753106d
Kelvin <vokemalosh32@gmail.com>
2022-06-18 18:08:21 +0300

    commit tree d646f0eebe5a8ec276151de06a1e8fba03815127

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // write a funtion that checks if a word is a palindrome 
+  //check if the first two letters is equal to the last two letters
+// find the length of a string
+// for( var i = word.length; i > 0; i-- )
+// {
+//     if( word[i] = word.charAt(word.length)-1 )
+//     {
+//         return true;
+//     }else{
+//         return false
+//     }
+// }
+var len = Math.floor(word.length / 2);
+  for (var i = 0; i < len; i++)
+    if (word[i] !== word[word.length - i - 1])
+      return false;
+  return true;
 }
 
 /* 
@@ -13,6 +30,7 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log (require.main)
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
